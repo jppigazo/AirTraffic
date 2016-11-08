435ed85b432dc2a0248171655c364a813b35f2ce
Solanya <paul.corlay@gmail.com>
2016-11-08 16:38:55 +0100

    Added PlaneBehaviour

---
+++ Assets/Script/PlaneBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlaneBehaviour : MonoBehaviour {
+
+    public System.String name;
+    public int altitude; // Conversion m / ft
+    public int destination;
+    public float vitesse; // Conversion knots (kts) / km.h / mph
+    public float direction;
+
+    public float angleVertical;
+
+    private float angleHorizontal;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 movement = Vector3.zero - transform.position; // Pour le moment on teste le mouvement vers le point 0 0
+        movement.Normalize();
+        transform.Translate(movement * vitesse);
+        angleVertical = Mathf.Atan2(movement.y, movement.x);
+    }
+}
