229c8bba966c1bb3b525762332e2ca9df1fdad36
jppigazo <jp.pigazo@gmail.com>
2016-11-08 16:31:34 +0100

    Create a plane
    Make a triangle which represent the plane.

---
+++ Assets/rotate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+    float rotateAngle;
+
+    GameObject triangle;
+    float angleVertical;
+
+	// Use this for initialization
+	void Start () {
+        triangle = gameObject.transform.parent.gameobject;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if( angleVertical != PlaneBehaviour.angleVertical)
+        {
+            angleVertical = PlaneBehaviour.angleVertical;
+            // change the angle to go to.
+            triangle.transform.rotation = angleVertical;
+        }
+	    
+	}
+}
