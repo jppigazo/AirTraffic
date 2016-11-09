728a6f103b2b989ecf4ec37178510dc72da59e38
Solanya <paul.corlay@gmail.com>
2016-11-09 12:16:30 +0100

    Spawning planes + arriving at center
    - Changed displayed speed
    - Added destination dictionnary

--- Assets/Script/PlaneBehaviour.cs
+++ Assets/Script/PlaneBehaviour.cs
@@ -2,7 +2,7 @@
 
 public class PlaneBehaviour : MonoBehaviour {
 
-    public System.String name;
+    public string planeName;
     public int altitude; // Conversion m / ft
     public int destination;
     public float vitesse; // Conversion knots (kts) / km.h / mph
@@ -11,6 +11,7 @@ public class PlaneBehaviour : MonoBehaviour {
     public float angleVertical;
 
     private float angleHorizontal;
+    private float realSpeed;
 
     // Use this for initialization
     void Start () {
@@ -19,9 +20,23 @@ public class PlaneBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        realSpeed = vitesse / 200000; // (from 1000 km.h to 0.005 speed)
+
         Vector3 movement = Vector3.zero - transform.position; // Pour le moment on teste le mouvement vers le point 0 0
-        movement.Normalize();
-        transform.Translate(movement * vitesse,Space.World);
-        angleVertical = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+        if (movement.magnitude < realSpeed)
+        {
+            GameObject.Find("GameManager").GetComponent<GameManager>().planesCount--;
+            Destroy(gameObject);
+        } else
+        {
+            movement.Normalize();
+            transform.Translate(movement * realSpeed, Space.World);
+            angleVertical = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+
+            Debug.Log("Name = " + planeName);
+            Debug.Log("Vitesse = " + vitesse);
+            Debug.Log("Altitude = " + altitude);
+        }
+        
     }
 }
