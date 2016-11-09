153b7c5d7c2bf065e0740afac732f5bc91cbdb2f
jppigazo <jp.pigazo@gmail.com>
2016-11-09 10:00:29 +0100

    Rotate sctipt up to date
    The rotate script follow the direction of the plane.

---
+++ Assets/Script/infoText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class infoText : MonoBehaviour {
+
+    private 
+	// Use this for initialization
+	void Start () {
+        TextMesh textObject = GameObject.Find("avion").GetComponent<TextMesh>();
+        textObject.text = "test";
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
