02759b304a2beca1de1ce91c9233ecf9f895d527
Solanya <paul.corlay@gmail.com>
2016-11-09 10:40:06 +0100

    Instantiation in GameManager

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -13,7 +13,9 @@ public class GameManager : MonoBehaviour {
 	void Start () {
 	    for (int i = 0; i < planesCount; i++)
         {
-            // GameObject instance = Instantiate(planeObject, new Vector3(Random.Range())
+            float radius = Random.Range(2.0f, 5.0f);
+            float angle = Random.Range(-Mathf.PI, Mathf.PI);
+            Instantiate(planeObject, new Vector3(radius * Mathf.Cos(angle), radius * Mathf.Sin(angle), 0.0f), Quaternion.Euler(0.0f, 180.0f, 0.0f));
         }
 	}
 	
