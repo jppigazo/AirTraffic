0094cd0054c89230381c54322e38660358824da7
Solanya <paul.corlay@gmail.com>
2016-11-09 09:58:07 +0100

    Added GameManager.cs

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public int planesCount;
+    public int planesMax;
+    public GameObject planeObject;
+
+    private
+
+	// Use this for initialization
+	void Start () {
+	    for (int i = 0; i < planesCount; i++)
+        {
+            GameObject instance = Instantiate(planeObject, new Vector3(Random.Range())
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
